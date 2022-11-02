efbaac90a0e2233f1dd9b923059860b3defd7819
nabilafkar <115811217+nabilafkar@users.noreply.github.com>
2022-11-02 15:45:18 +0700

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Database {
+    // TODO Create List of Menu Object to Store Menu from Menu Class
+    
+    List<Menu> listMenu = new ArrayList<Menu>();
+
+    // TODO Create Method to Insert Menu to Database
+    public void insertMenu(Menu menu){
+        
+    }
+    // TODO Create Method to Show Menu from Database
+    public void showMenu(Menu menu){
+
+    }
+    // TODO Create Method to Search Menu from Database
+    public void searchMennu(Menu menu){
+        
+    }
+
+}
---
+++ User.java
@@ -0,0 +1,19 @@
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String name;
+    int phone;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+    public void register(){
+        System.out.println("Success");
+    }
+
+}
